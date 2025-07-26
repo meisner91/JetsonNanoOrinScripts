61275fde87d38408629af8502622f66799a27fd0
meisner91 <40890089+meisner91@users.noreply.github.com>
2025-07-26 14:15:26 +0200

    Create AfterInstall.sh

---
+++ AfterInstall.sh
@@ -0,0 +1,10 @@
+# Add workspaces to the bash so that Terminals will recognize them.
+
+# ROS2 Humble setup.bash
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+
+# Your own Workspace 
+echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
+
+# First, source #ROS2, then your workspace. Check with:
+nano ~/.bashrc
