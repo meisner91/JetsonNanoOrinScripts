b577a80a72febfc6a2c5865d7f72556d22da5a6e
meisner91 <40890089+meisner91@users.noreply.github.com>
2025-11-01 16:03:44 +0100

    Add script to fix Snapd application startup issues
    Added script to install an older version of Snapd for Jetpack.

--- 00_Jetson_Setup_SNAPD.sh
+++ 00_Jetson_Setup_SNAPD.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+# ---- After a fresh Installation of Jetpack Snapd -> Issue Snapd Applications wont Start
+# Simple fast fix is to use an older Snapd Version 
 
+sudo snap download snapd --revision=24724
+sudo snap ack snapd_24724.assert
+sudo snap install snapd_24724.snap
+sudo snap refresh --hold snapd
+
+# Apps like Chromium or Firefox should now Start
